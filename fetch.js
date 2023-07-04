ca3853998a8f42dab2e049fe3c2ff98a408c3249
yoh1604 <115641112+yoh1604@users.noreply.github.com>
2023-07-04 21:32:31 +0700

    first commit

---
+++ fetch.js
@@ -0,0 +1,6 @@
+fetch('https://jsonplaceholder.typicode.com/posts/5')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(error => console.log(error));
+
+console.log(fetch = require('node-fetch'));
